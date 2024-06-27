a717a4381982e3a749e5b270b5312a76f59f9cd6
Dan Kuklianov <30766104+danlizard@users.noreply.github.com>
2024-06-27 20:05:34 +0300

    vanilla fish

--- src/main/java/com/scalyhat/pfac/pfac.java
+++ src/main/java/com/scalyhat/pfac/pfac.java
@@ -1,23 +1,15 @@
 package com.scalyhat.pfac;
 
-import java.util.Set;
-
-import com.scalyhat.pfac.providers.DynamicLanguageProvider;
-
-import net.minecraft.core.RegistrySetBuilder;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceKey;
+import net.minecraft.client.gui.font.glyphs.BakedGlyph.Effect;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -49,8 +41,15 @@ public class pfac
     public static final RegistryObject<Attribute> CAN_PLAY_SOUNDBITE = ATTRIBUTES.register("fishe_sound_playable", () -> 
         new CustomAttribute("Whether this entity can play the Fishe soundbite", 1).setSyncable(true));
 
-    public static final RegistryObject<Item> LE_FISHE_AU_CHOCOLAT = ITEMS.register("le_fishe_au_chocolat", () ->
-        new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder().build())));
+    public static final RegistryObject<Item> LE_COD_AU_CHOCOLAT = ITEMS.register("le_cod_au_chocolat", () ->
+        new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(7).saturationMod(0.5f).build())));
+    public static final RegistryObject<Item> LE_SALMON_AU_CHOCOLAT = ITEMS.register("le_salmon_au_chocolat", () ->
+        new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(7).saturationMod(0.5f).build())));
+    public static final RegistryObject<Item> LE_PUFFERFISH_AU_CHOCOLAT = ITEMS.register("le_pufferfish_au_chocolat", () ->
+        new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(0.4f).effect(
+            () -> new MobEffectInstance(MobEffects.POISON, 120, 3), 1).build())));
+    public static final RegistryObject<Item> LE_TROPICAL_FISH_AU_CHOCOLAT = ITEMS.register("le_tropical_fish_au_chocolat", () ->
+        new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.4f).build())));
 
     public pfac()
     {
@@ -66,47 +65,13 @@ public class pfac
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    // @SubscribeEvent
-    // public void gatherData(GatherDataEvent event) {
-    //     event.getGenerator()
-    //     .addProvider(
-    //         event.includeServer(), 
-    //         output -> new DynamicLanguageProvider(
-    //             output,
-    //             MODID,
-    //             "en-us"
-    //         )
-    //     )
-    //     .addProvider(
-    //         event.includeServer(), 
-    //         output -> new DatapackBuiltinEntriesProvider(
-    //             output,
-    //             event.getLookupProvider(),
-    //             new RegistrySetBuilder().add(Registries.ITEM, bootstrap -> {
-    //                 for (Item added : Config.fisheItems) {
-    //                     FoodProperties currentFood = added.getFoodProperties(null, null);
-    //                     Integer nutrition = currentFood == null ? 5 : currentFood.getNutrition();
-    //                     Float saturationMod = currentFood == null ? 0.4f : currentFood.getSaturationModifier();
-
-    //                     ResourceKey<Item> currentItem = ResourceKey.create(Registries.ITEM, new ResourceLocation(MODID, added.toString().concat("_au_chocolat")));
-    //                     bootstrap.register(
-    //                         currentItem, 
-    //                         new GenericFisheItem(new Item.Properties().food(new FoodProperties.Builder()
-    //                             .nutrition(nutrition)
-    //                             .saturationMod(saturationMod)
-    //                             .build()
-    //                         )));
-    //                 }
-    //             }),
-    //             Set.of(MODID)
-    //         )
-    //     );
-    // }
-
     @SubscribeEvent
     public void addToTabs(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
-            event.accept(LE_FISHE_AU_CHOCOLAT);
+            event.accept(LE_COD_AU_CHOCOLAT);
+            event.accept(LE_SALMON_AU_CHOCOLAT);
+            event.accept(LE_TROPICAL_FISH_AU_CHOCOLAT);
+            event.accept(LE_PUFFERFISH_AU_CHOCOLAT);
         }
     }
 
